356bee8c76fef4ada5fc31f47436e2797216a01f
Mohamed-Ilyas Bouliri <moboulir@c3r7p4.1337.ma>
2026-07-22 20:41:06 +0100

    done

--- pathfinding.py
+++ pathfinding.py
@@ -1,26 +1,59 @@
-from collections import deque 
-from grapth import Graph
+import heapq
+from graph import Graph
 from zone import Zone
 
-def bfs(graph: Graph, start: Zone, end: Zone) -> list[Zone]:
-    queue = deque([start])   # Create queue and put 'start' inside
-    visited = {start}        # Create set and put 'start' inside
-    parent = {start: None}   # Create dict and map 'start' to None
+def zone_cost(zone: Zone) -> int:
+    result = 100
+    if zone.zone_type == "priority":
+        result = 99
+    elif zone.zone_type == "restricted":
+        result = 200
+    return result
+
+def dijkstra(graph: Graph, start: Zone, end: Zone) -> list[Zone]:
+    # 1. SETUP
+    pq = [(0, start)]
+    distance = {start: 0}
+    parent = {start: None}
+
+    # 2. THE ENGINE
+    while pq:
+        current_cost, current = heapq.heappop(pq)
+        
+        # The Trash Can: skip stale tickets with old, bad costs
+        if current_cost > distance.get(current, float('inf')):
+            continue
+
+        # If we hit the goal, stop spreading!
+        if current == end:
+            break
+
+        neighbors_zones = graph.get_neighbors(current)
+
+        # Check all neighbors
+        for neighbor in neighbors_zones:
+            old_cost = distance.get(neighbor, float('inf'))
+            new_cost = current_cost + zone_cost(neighbor)
+
+            # If we found a cheaper route, update everything
+            if new_cost < old_cost:
+                distance[neighbor] = new_cost
+                parent[neighbor] = current
+                heapq.heappush(pq, (new_cost, neighbor))
+
+    # 3. PATH RECONSTRUCTION
+    # Protect against KeyError if the end zone is unreachable
+    if end not in parent:
+        return []
+
+    # Walk backward from the end to the start
+    path = []
+    step = end
+    while step is not None:
+        path.append(step)
+        step = parent[step]
     
-    # What comes next?
-    #the loop  for queue
-
-    while queue:
-        current  = queue.popleft()
-        neighbors = graph.get_neighbors(current)
-        for neighbor in neighbors:
-            if neighbor not in visited:
-                visited.append(current)
-                parent[neighbor]= current
-                queue.add(neighbor)
-
-The Loop: While the queue is not empty, popleft() the current zone.
-
-The Goal Check: If the current zone is the end zone, you found the shortest path! (For now, you can just pass or write a comment here—we can build the path-reconstruction step afterward).
-
-The Neighbors: Loop through the neighbors of the current zone. Apply the Golden Rule: If a neighbor is not in visited, mark it visited, record its parent, and append it to the queue.
+    # Flip it so it goes Start -> End
+    path.reverse()
+    
+    return path
